ae3b51e9eaec091288c3fea7793a2577f5a111e6
iurena94 <isaiasurena94@gmail.com>
2023-11-13 20:47:39 -0500

    Added functions to views. Created Admin account. Added a new model. Created info.py for emails.

--- mysite/hrisapp/views.py
+++ mysite/hrisapp/views.py
@@ -1,17 +1,40 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("employeeid")
+        password = "ps123"
+        myuser = User.objects.create_user(username, password)
+        myuser.save()
+        return redirect("thankyou.html")
     return render(request, "signup.html")
 
 def thankyou(request):
     return render(request, "thankyou.html")
 
 def login(request):
+    if request.method == 'POST':
+        usern = request.POST.get("employeeid")
+        pw = request.POST.get("password")
+        user = authenticate(username=usern, password=pw)
+        if user is not None:
+            login(request, user)
+            fname = usern
+            return render(request, "dashboard.html", {'fname':fname})
+        else:
+            messages.error(request, "Bad Credentials!")
+            return redirect("login")
     return render(request, "index.html")
 
 def dashboard(request):
     return render(request, "dashboard.html") 
+
+def contacts(request):
+    return render(request, "")
---
+++ mysite/mysite/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "HRIS.Authentication@gmail.com"
+EMAIL_HOST_PASSWORD = "cis5800pw"
+EMAIL_PORT = 587
